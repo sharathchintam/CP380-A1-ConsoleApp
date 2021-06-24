991b772bec0264d6e78a3406b19e6484fd64db0a
sharathchintam <65326321+sharathchintam@users.noreply.github.com>
2021-06-24 15:45:40 -0400

    added code

--- RatingAdjustmentService/RatingAdjustmentService.cs
+++ RatingAdjustmentService/RatingAdjustmentService.cs
@@ -22,6 +22,7 @@ namespace RatingAdjustment.Services
         void SetPercentPositive(double stars)
         {
             // TODO: Implement this!
+            _percent_positive = stars / MAX_STARS;
         }
 
         /**
@@ -30,6 +31,7 @@ namespace RatingAdjustment.Services
         void SetQ(double number_of_ratings)
         {
          // TODO: Implement this!
+         _q = Z * Math.Sqrt((_percent_positive * (1- _percent_positive) + (Math.Pow(Z,2) / (4 * number_of_ratings)) )/number_of_ratings);
         }
 
         /** Adjusted lower bound
@@ -40,7 +42,11 @@ namespace RatingAdjustment.Services
          */
         public double Adjust(double stars, double number_of_ratings) {
             // TODO: Implement this!
-            return 0.0;
+            SetPercentPositive(stars);
+            SetQ(number_of_ratings);
+
+            double lb = (_percent_positive + (Math.Pow(Z,2)/ (2 * number_of_ratings)) - _q) / (1 + (Math.Pow(Z,2) / number_of_ratings));
+            return lb * 5;  
         }
     }
 }
